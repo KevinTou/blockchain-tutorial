579e76d80070b0946af540f3efa8200c60dbd17b
Kevin Tou <toukevin@gmail.com>
2020-01-15 06:19:54 -0800

    Added to hash and last_block method

--- blockchain.py
+++ blockchain.py
@@ -1,3 +1,8 @@
+import hashlib
+import json
+from time import time
+
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
@@ -48,9 +53,17 @@ class Blockchain(object):
     @staticmethod
     def hash(block):
         # Hashes a Block
-        pass
+        """
+        Creates a SHA-256 hash of a Block
+        :param block: <dict> Block
+        :return: <str>
+        """
+
+        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
         # Returns the last Block in the chain
-        pass
+        return self.chain[-1]
